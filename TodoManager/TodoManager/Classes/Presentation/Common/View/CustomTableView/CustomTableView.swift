af46073dc0af6f5cd1bdc60d47c9dce5514a43ac
Anton Polyakov <a.polyakov@redmadrobot.com>
2018-05-12 15:47:46 +0300

    Bugfix missing CustomTableView

---
+++ TodoManager/TodoManager/Classes/Presentation/Common/View/CustomTableView/CustomTableView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableView.swift
+//  TodoManager
+//
+//  Created by Anton Polyakov on 12/05/2018.
+//  Copyright © 2018 ton252. All rights reserved.
+//
+
+import UIKit
+
+final class CustomTableView: UIView {
+    
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet var tableView: UITableView!
+    
+}
