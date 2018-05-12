18437c5da8bfc1cf81f2e868876acd3fa6423c9f
Anton Polyakov <a.polyakov@redmadrobot.com>
2018-05-12 15:23:56 +0300

    Add TasksViewController, CustomTableView

---
+++ TodoManager/TodoManager/Classes/Presentation/Main/View Controller/TasksViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  TodoManager
+//
+//  Created by Anton Polyakov on 12/05/2018.
+//  Copyright © 2018 ton252. All rights reserved.
+//
+
+import UIKit
+
+class TasksViewController: CustomViewController<CustomTableView> {
+    
+}
