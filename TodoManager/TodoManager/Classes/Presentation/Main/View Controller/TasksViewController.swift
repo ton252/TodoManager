21ded250f49505db6ec2a2df56411e0ea2a26f3b
Anton Polyakov <a.polyakov@redmadrobot.com>
2018-05-12 15:40:39 +0300

    Add UISegmentedControl and UIBarButtonItem to TasksViewController

--- TodoManager/TodoManager/Classes/Presentation/Main/View Controller/TasksViewController.swift
+++ TodoManager/TodoManager/Classes/Presentation/Main/View Controller/TasksViewController.swift
@@ -10,4 +10,44 @@ import UIKit
 
 class TasksViewController: CustomViewController<CustomTableView> {
     
+    
+    // MARK: - ViewController LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    
+    // MARK: - Configure UI
+    
+    private func configureUI() {
+        addSegmentControll()
+        addAddBarButtonItem()
+    }
+    
+    private func addSegmentControll() {
+        let segment: UISegmentedControl = UISegmentedControl(items: ["Upcoming", "Completed"])
+        segment.sizeToFit()
+        segment.selectedSegmentIndex = 0
+        navigationItem.titleView = segment
+        segment.addTarget(self, action: #selector(segmentedValueChanged(_:)), for: .valueChanged)
+    }
+    
+    private func addAddBarButtonItem() {
+        let item = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addNewNoteTap))
+        navigationItem.setRightBarButton(item, animated: false)
+    }
+    
+    @objc private func addNewNoteTap() {
+        print("addNewNoteTap")
+    }
+    
+    @objc private func segmentedValueChanged(_ segment: UISegmentedControl) {
+        print(segment.selectedSegmentIndex)
+    }
+    
 }
